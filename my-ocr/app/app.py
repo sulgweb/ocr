2d0270c7aa40d33e93cb2c1de7fd54ef72498d9e
冼鹏飞 <xianpengfei@cvte.com>
2022-06-11 12:07:56 +0800

    🐛 移除语音识别

--- my-ocr/app/app.py
+++ my-ocr/app/app.py
@@ -3,7 +3,7 @@ Description:
 Author: xianpengfei
 LastEditors: xianpengfei
 Date: 2022-05-26 21:02:23
-LastEditTime: 2022-06-08 01:03:42
+LastEditTime: 2022-06-11 12:07:53
 '''
 from paddleocr import PaddleOCR, draw_ocr
 import os, base64
@@ -15,11 +15,12 @@ from paddlespeech.cli.text import TextExecutor
 from paddlespeech.server.bin.paddlespeech_client import ASROnlineClientExecutor
 
 
+
 # 图片识别
 def distinguish_img(img_path):
     # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
     # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
-    ocr = PaddleOCR(use_angle_cls=True, lang="ch",enable_mkldnn=False, use_gpu=False, use_tensorrt= True)  # need to run only once to download and load model into memory
+    ocr = PaddleOCR(use_angle_cls=True, lang="ch",enable_mkldnn=True, use_gpu=False, use_tensorrt= True)  # need to run only once to download and load model into memory
     result = ocr.ocr(img_path, cls=True)
     return result
     os.remove(img_path)
@@ -43,7 +44,7 @@ class ImageView(Resource):
     def get(self):
         return {'code':200,'msg':'plese use post method!!!'}
     def post(self):
-        file_path = './image/test.png'
+        file_path = 'app/image/test.png'
         images = request.json.get('images')
         datas = []
         for item in images:
@@ -52,42 +53,8 @@ class ImageView(Resource):
             datas.append(res)
         return {'code': 200, 'msg': 'ok', 'data': str(datas)}
 
-class AudioView(Resource):
-  def get(self):
-    device = paddle.device.get_device()
-    asr_executor = ASRExecutor()
-    text = asr_executor(
-        model='conformer_wenetspeech',
-        lang='zh',
-        sample_rate=16000,
-        config=None,  # Set `config` and `ckpt_path` to None to use pretrained model.
-        ckpt_path=None,
-        audio_file='./audio/001.wav',
-        force_yes=True,
-        device=device)
-        
-    print('ASR Result: \n{}'.format(text))
-    return {'code': 200, 'msg': 'ok', 'data': str(text)}
-
-
-class PunctuationView(Resource):
-  def get(self):
-    text_executor = TextExecutor()
-    result = text_executor(
-        text='今天的天气真不错啊你下午有空吗我想约你一起去吃饭',
-        task='punc',
-        model='ernie_linear_p3_wudao',
-        lang='zh',
-        config=None,
-        ckpt_path=None,
-        punc_vocab=None,
-        device=paddle.get_device())
-    print('Text Result: \n{}'.format(result))
-    return {'code': 200, 'msg': 'ok', 'data': str(result)}
-
 
 api.add_resource(ImageView, '/image-to-text')
-api.add_resource(AudioView, '/audio-to-text')
-api.add_resource(PunctuationView, '/replay-punctuation')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8866)
